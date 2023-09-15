c30446b2c840678bb5e4984c125cc75e1573e80d
Keli06001 <keli06001@utb.vaxjo.se>
2023-09-15 13:18:23 +0200

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in lön 1");
+            string ettText = Console.ReadLine();
+            int ett = int.Parse(ettText);
+            Console.WriteLine("Skriv in lön 2");
+            string tvåText= Console.ReadLine();
+            int två=int.Parse(tvåText);
+            Console.WriteLine("Skriv in lön 3");
+            string treText = Console.ReadLine();
+            int tre = int.Parse(treText);
+            int medel = (ett + två + tre) / 3;
+            Console.WriteLine("Medel lönen är "+medel + "kr");
+        }
+    }
+}
